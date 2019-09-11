73b6ad11747b9d317a7764f6967e0e29a49702ea
Petros H Melake <44558167+Petivip@users.noreply.github.com>
2019-09-10 20:23:19 -0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,72 @@
+body{
+ background-color: #DED6D6;
+ background-size: 30%;
+ color: #A14040;
+ font-family:"montserrat";
+}
+.container{
+  text-align: center; 
+}
+#head{
+  font-size: 3em;
+  color: #670F0F;
+  font-weight: 700;
+  
+}
+
+h4{
+  display: inline;
+  font-size: 2em;
+}
+
+#breanum, #sessnum{
+  font-size: 3.5em;
+  font-weight: 600;
+ 
+ 
+}
+
+.btn{
+   background: none;
+      outline: none;
+      border: none;
+      color: #937C9F;
+      cursor: pointer;
+}
+.btn1{
+  color:#825C98;
+}
+
+#titlesess{
+  font-size: 2em;
+  color: #670F0F;
+  font-weight: 600;
+}
+#titlebrea{
+  font-size: 2em;
+  color: #670F0F;
+  font-weight: 600;
+}
+
+.prev{
+  margin-top: 150px;
+ 
+}
+#title{
+   font-size: 3.5em;
+  font-weight: 600;
+  color:#6669B9;
+}
+#counter{
+    font-size: 3.5em;
+  color: #44468C;
+  font-weight: 600;
+}
+  
+.author {
+  text-align: center;
+  font-family: Share Tech Mono, sans;
+  margin-top: 15px;
+  font-size: 14px;
+  color: #a50d0d;
+}
